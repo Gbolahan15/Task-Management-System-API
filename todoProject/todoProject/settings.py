2fe48d8e93289f70c6d11f84571e091ad1aa3596
Gbolahan <adesinagbolahan15@gmail.com>
2026-06-01 13:34:01 +0100

    fix: correct todoApp app config for deployment

--- todoProject/todoProject/settings.py
+++ todoProject/todoProject/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todoApp',
+    'todoApp.apps.TodoappConfig',
     'rest_framework',
     'django_filters',
     'drf_yasg',
